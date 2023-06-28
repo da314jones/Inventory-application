8c79a4b6bd9d456bed149f7a1f1eb4e0022e4516
da314jones <dwaynejones@pursuit.org>
2023-06-27 21:39:22 -0400

    Lay out & pseudo code

---
+++ index.js
@@ -0,0 +1,37 @@
+/*select the elements from the dom
+h1 
+input
+body
+main
+
+set up header and create a container for resource list
+ul form
+
+append fields and submit button to form 
+append input type text
+input placeholder
+input name
+
+append form
+body
+
+write functions to generate a  new resource list
+
+add delete button to each list item
+
+add event listeners to delete button
+
+add event listeners to click 
+
+add event listeners to submit 
+
+add event listeners to cancel 
+
+get value of input fields and log(event target)
+
+generate new list item
+
+add event listeners to reset 
+reset form
+append form to body
+*/
