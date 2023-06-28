ee7a702ed7d062726945bcbb1c479121d892152d
da314jones <dwaynejones@pursuit.org>
2023-06-27 22:14:43 -0400

    updated peusdo code

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
-/*select the elements from the dom
+/*set up html structure
+select the elements from the dom
 h1 
 input
 body
 main
+define variables
+empty arrays for or objects to store data
+this will hold inventory info for each item
+
 
 set up header and create a container for resource list
 ul form
+write functions for form submission 
+event listener for submit 
+retrieve value entered in input field when form is submitted
 
+ create function to generate  inventory cards
+ iterate thru inventory for each item create card element
+ set card to display info
+ append card to container
+ 
+ add delete button to each list item
+ 
+ add event listeners to delete button
+ 
+ add event listeners to click 
+ 
+ add event listeners to submit 
+ 
+ add event listeners to cancel 
+ 
+ get value of input fields and log(event target)
+ 
+ generate new list item
+
+ write functions to generate a  new resource list
+ initialize app
+  call functions generate cards existing 
 append fields and submit button to form 
 append input type text
 input placeholder
 input name
 
+
+ test 
 append form
 body
 
-write functions to generate a  new resource list
 
-add delete button to each list item
-
-add event listeners to delete button
-
-add event listeners to click 
-
-add event listeners to submit 
-
-add event listeners to cancel 
-
-get value of input fields and log(event target)
-
-generate new list item
 
 add event listeners to reset 
 reset form
